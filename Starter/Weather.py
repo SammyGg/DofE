178385f89223d0a6fe17878ee8395f6bdae7d9e4
SammyGg <samuelgascoyne@outlook.com>
2023-11-20 19:43:55 +0000

    20/11 Weather Program

--- Starter/Weather.py
+++ Starter/Weather.py
@@ -2,10 +2,10 @@ import random
 temp = int(random.choice(["10","12","14","16","18","20","22"]))
 forecast = random.choice  (["cloudy","sunny","rainy"])
 if temp >= 14 and forecast == "cloudy":
-    print("Wear a thick winter coat it may be chilly")
+    print(f"The Temp is {temp} and the forecast is {forecast}. Wear a thick winter coat it may be chilly")
 elif temp <= 16 and forecast == "cloudy":
-    print("It's starting to get quite warm! Still rememeber to wear long sleeves though")
+    print(f"The Temp is {temp} and the forecast is {forecast}.It's starting to get quite warm! Still rememeber to wear long sleeves though")
 elif temp <= 20 and forecast == "sunny":
-    print("It's lovely weather wear some nice short sleaves")
+    print(f"The Temp is {temp} and the forecast is {forecast}.It's lovely weather wear some nice short sleaves")
 elif temp <= 15 and forecast == "rainy":
-    print("You don't need a thick winter coat. Rememeber to wear a rain coat though")
+    print(f"The Temp is {temp} and the forecast is {forecast}.You don't need a thick winter coat. Rememeber to wear a rain coat though")
